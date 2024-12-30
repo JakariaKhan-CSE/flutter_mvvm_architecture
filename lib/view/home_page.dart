a59f044c2a284446fd54efdaf6a81a46a62c9dbc
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2024-12-30 15:36:06 +0600

    utils complete.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/utils.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,6 +14,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink.withAlpha(150),
+      body: Center(
+        child: InkWell(
+            onTap: (){
+              Utils.snackBarMessage(context, 'This is test snackbar message');
+
+            },
+            child: Text('Show snackbar',style: TextStyle(color: Colors.white,fontSize: 22),)),
+      ),
     );
   }
 }
